8f3f766bad9020322d4e969218c39f182abf710c
BrukAnn <kennyman143@gmail.com>
2021-08-25 21:56:12 +0300

    test fixing

--- pages/tplvlmenu_tab.rb
+++ pages/tplvlmenu_tab.rb
@@ -1,17 +1,17 @@
 class TPLMTab < BasePage
   def women
-    browser.find_element(name: 'Women')
+    browser.find_element(xpath: '//ul[@class="sf-menu clearfix menu-content sf-js-enabled sf-arrows"]/li/a[@title="Women"]')
   end
 
   def dresses
-    browser.find_element(name: 'Dresses')
+    browser.find_element(xpath: '//ul[@class="sf-menu clearfix menu-content sf-js-enabled sf-arrows"]/li/a[@title="Dresses"]')
   end
 
   def tshirts
-    browser.find_element(name: 'T-shirts')
+    browser.find_element(xpath: '//ul[@class="sf-menu clearfix menu-content sf-js-enabled sf-arrows"]/li/a[@title="T-shirts"]')
   end
 
   def tplm_bar
-    browser.find_element(class: 'sf-menu clearfix menu-content sf-js-enabled sf-arrows')
+    browser.find_element(xpath: '//ul[@class="sf-menu clearfix menu-content sf-js-enabled sf-arrows"]')
   end
 end
