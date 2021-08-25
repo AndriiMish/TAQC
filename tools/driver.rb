9baa128be1669e8b9d5c0a24af0b0c7071b7609f
Muzhchinchik <73602159+Muzhchinchik@users.noreply.github.com>
2021-08-25 19:22:49 +0300

    fixed

--- tools/driver.rb
+++ tools/driver.rb
@@ -2,7 +2,7 @@ require 'selenium/webdriver'
 
 class Driver
   def initialize
-    $driver = Selenium::WebDriver.for :firefox
+    $driver = Selenium::WebDriver.for(:firefox)
     $driver.manage.timeouts.implicit_wait = 10 # seconds
   end
 end
