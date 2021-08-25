9db57f9895ea30af9ba003515cf5dfd9de1540fb
Muzhchinchik <73602159+Muzhchinchik@users.noreply.github.com>
2021-08-25 21:53:28 +0300

    first test contact us page

--- pages/landing_page.rb
+++ pages/landing_page.rb
@@ -2,4 +2,7 @@ class LandingPage
   def sign_in_button
     browser.find_element(class: 'login')
   end
+  def contact_us_button
+    browser.find_element(xpath: '//*[@id="contact-link"]/a')
+  end
 end
