c426065555138cf85a451fa6503679971086ca00
Kytaihora Volodymyr <kytaihora.v@gmail.com>
2021-08-30 13:51:13 +0300

    Add options

--- pages/women_page.rb
+++ pages/women_page.rb
@@ -5,7 +5,8 @@ class WomenPage
         #TBD to move to parent page
         browser.find_element(class: 'cat-name')
     end
-# TITLES OF CATEGORIES
+
+#TITLES OF CATEGORIES
     def categories_filter
         browser.find_element(xpath: "//span[contains(@class,'layered_subtitle') and text()='Categories']")
     end
@@ -41,7 +42,8 @@ class WomenPage
     def condition_filter
         browser.find_element(xpath: "//span[contains(@class,'layered_subtitle') and text()='Condition']")
     end
-# OPTIONS OF CATEGORIES
+
+#OPTIONS OF CATEGORIES
     def categories_filter_tops
         browser.find_element(xpath: "//label/a[text()='Tops']")
     end
@@ -50,6 +52,7 @@ class WomenPage
         browser.find_element(xpath: "//label/a[text()='Dresses']")
     end
 
+#OPTIONS OF SIZE
     def size_filter_s
         browser.find_element(xpath: "//label/a[text()='S']")
     end
@@ -61,5 +64,104 @@ class WomenPage
     def size_filter_l
         browser.find_element(xpath: "//label/a[text()='L']")
     end
+
+# OPTIONS OF COLOR
+def color_filter_beige
+    browser.find_element(xpath: "//label/a[text()='Beige']")
+end
+
+def color_filter_white
+    browser.find_element(xpath: "//label/a[text()='White']")
+end
+
+def color_filter_black
+    browser.find_element(xpath: "//label/a[text()='Black']")
+end
+
+def color_filter_orange
+    browser.find_element(xpath: "//label/a[text()='Orange']")
+end
+
+def color_filter_blue
+    browser.find_element(xpath: "//label/a[text()='Blue']")
+end
+
+def color_filter_green
+    browser.find_element(xpath: "//label/a[text()='Green']")
+end
+
+def color_filter_yellow
+    browser.find_element(xpath: "//label/a[text()='Yellow']")
+end
+
+def color_filter_pink
+    browser.find_element(xpath: "//label/a[text()='Pink']")
+end
+
+# OPTIONS OF COMPOSITIONS
+def compositions_filter_cotton
+    browser.find_element(xpath: "//label/a[text()='Cotton']")
+end
+
+def compositions_filter_polyester
+    browser.find_element(xpath: "//label/a[text()='Polyester']")
+end
+
+def compositions_filter_viscose
+    browser.find_element(xpath: "//label/a[text()='Viscose']")
+end
+
+# OPTIONS OF STYLES
+def styles_filter_casual
+    browser.find_element(xpath: "//label/a[text()='Casual']")
+end
+
+def styles_filter_dressy
+    browser.find_element(xpath: "//label/a[text()='Dressy']")
+end
+
+def styles_filter_girly
+    browser.find_element(xpath: "//label/a[text()='Girly']")
+end
+
+# OPTIONS OF PROPERTIES
+def properties_filter_colorfulDress
+    browser.find_element(xpath: "//label/a[text()='Colorful Dress']")
+end
+
+def properties_filter_maxiDress
+    browser.find_element(xpath: "//label/a[text()='Maxi Dress']")
+end
+
+def properties_filter_midiDress
+    browser.find_element(xpath: "//label/a[text()='Midi Dress']")
+end
+
+def properties_filter_shortDress
+    browser.find_element(xpath: "//label/a[text()='Short Dress']")
+end
+
+def properties_filter_shortSleeve
+    browser.find_element(xpath: "//label/a[text()='Short Sleeve']")
+end
+
+# OPTIONS OF AVAILABILITY
+def availability_filter_inStock
+    browser.find_element(xpath: "//label/a[text()='In stock']")
+end
+
+# OPTIONS OF MANUFACTURER
+def manufacturer_filter_fashionManufacturer
+    browser.find_element(xpath: "//label/a[text()='Fashion Manufacturer']")
+end
+
+# OPTIONS OF CONDITION
+def condition_filter_new
+    browser.find_element(xpath: "//label/a[text()='New']")
+end
+
+# OPTIONS OF PRICE
+
+#????????????????
 
 end
--- spec/test/women_page_ui_spec.rb
+++ spec/test/women_page_ui_spec.rb
@@ -11,44 +11,61 @@ RSpec.describe 'Women page' do
 
     after(:all) { browser.close }
 
+    
     it 'verifies title displayed' do
       expect(women_page.page_title.text).to include(WomenPage::PAGE_TITLE)
     end
 
-    it 'verifies categories filter displayed' do
-      expect(women_page.categories_filter.displayed?).to eq(true)
+  context 'open with filters' do
+
+    it 'verifies categories filter displayed with options' do
+      expect(women_page.categories_filter.text).to eq("Categories")
+      expect(women_page.categories_filter_tops.text).to include("Tops")
+      expect(women_page.categories_filter_dresses.text).to include("Dresses")
     end
 
-    it 'verifies size filter displayed' do
-      expect(women_page.size_filter.displayed?).to eq(true)
+    it 'verifies size filter displayed with options' do
+      expect(women_page.size_filter.text).to eq("Size")
+      expect(women_page.size_filter_s.text).to include("S")
+      expect(women_page.size_filter_m.text).to include("M")
+      expect(women_page.size_filter_l.text).to include("L")
     end
 
-    it 'verifies color filter displayed' do
-      expect(women_page.color_filter.displayed?).to eq(true)
+    it 'verifies color filter displayed with options' do
+      expect(women_page.color_filter.text).to eq("Color")
     end
 
-    it 'verifies compositions filter displayed' do
-      expect(women_page.compositions_filter.displayed?).to eq(true)
+    it 'verifies compositions filter displayed with options' do
+      expect(women_page.compositions_filter.text).to eq("Compositions")
+      expect(women_page.compositions_filter_cotton.text).to include("Cotton")
+      expect(women_page.compositions_filter_polyester.text).to include("Polyester")
+      expect(women_page.compositions_filter_viscose.text).to include("Viscose")
     end
 
-    it 'verifies styles filter displayed' do
-      expect(women_page.styles_filter.displayed?).to eq(true)
+    it 'verifies styles filter displayed with options' do
+      expect(women_page.styles_filter.text).to eq("Styles")
+      expect(women_page.styles_filter_casual.text).to include("Casual")
+      expect(women_page.styles_filter_dressy.text).to include("Dressy")
+      expect(women_page.styles_filter_girly.text).to include("Girly")
     end
 
     it 'verifies properties filter displayed' do
       expect(women_page.properties_filter.displayed?).to eq(true)
     end
 
-    it 'verifies availability filter displayed' do
-      expect(women_page.availability_filter.displayed?).to eq(true)
+    it 'verifies availability filter displayed with options' do
+      expect(women_page.availability_filter.text).to eq("Availability")
+      expect(women_page.availability_filter_inStock.text).to include("In stock")
     end
 
-    it 'verifies manufacturer filter displayed' do
-      expect(women_page.manufacturer_filter.displayed?).to eq(true)
+    it 'verifies manufacturer filter displayed with options' do
+      expect(women_page.manufacturer_filter.text).to eq("Manufacturer")
+      expect(women_page.manufacturer_filter_fashionManufacturer.text).to include("Manufacturer")
     end
 
-    it 'verifies condition filter displayed' do
-      expect(women_page.condition_filter.displayed?).to eq(true)
+    it 'verifies condition filter displayed with options' do
+      expect(women_page.condition_filter.text).to eq("Condition")
+      expect(women_page.condition_filter_new.text).to include("New")
     end
-
+  end
 end
