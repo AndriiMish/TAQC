8e108ebd9ba89d1344942a0007a20d90aa490f98
Muzhchinchik <73602159+Muzhchinchik@users.noreply.github.com>
2021-08-30 14:06:50 +0300

    test added

--- pages/contact_us_page.rb
+++ pages/contact_us_page.rb
@@ -8,4 +8,28 @@ class ContactUsPage
     browser.current_url.include? 'contact'
   end
 
+  def message_field
+    browser.find_element(id: 'message')
+  end
+
+  def email_field
+    browser.find_element(id: 'email')
+  end
+
+  def id_order_field
+    browser.find_element(id: 'id_order')
+  end
+
+  def file_upload_field
+    browser.find_element(id: 'uniform-fileUpload')
+  end
+
+  def selector_field
+    browser.find_element(id: 'uniform-id_contact')
+  end
+
+  def send_button
+    browser.find_element(id: 'submitMessage')
+  end
+
 end
--- spec/test/contact_us_ui.rb
+++ spec/test/contact_us_ui.rb
@@ -14,5 +14,31 @@ RSpec.describe 'Contact us page' do
     expect(contact_us_page.on_page?).to eq(true)
   end
 
+  it 'verifyes that message field is displayed' do
+    expect(contact_us_page.message_field.displayed?).to eq(true)
+  end
+
+  it 'verifyes that email field is displayed' do
+    expect(contact_us_page.email_field.displayed?).to eq(true)
+  end
+
+  it 'verifyes that Order reference field is displayed' do
+    expect(contact_us_page.id_order_field.displayed?).to eq(true)
+  end
+
+  it 'verifyes that file upload field is displayed' do
+    expect(contact_us_page.file_upload_field.displayed?).to eq(true)
+  end
+
+  it 'verifyes that selector is displayed' do
+    expect(contact_us_page.selector_field.displayed?).to eq(true)
+  end
+
+  it 'verifyes that Send button is displayed' do
+    expect(contact_us_page.send_button.displayed?).to eq(true)
+  end
+
+
+
   end
 
