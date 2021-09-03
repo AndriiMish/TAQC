d3f36144b7ca6ccf9ae9f56f769c8627821f941d
Muzhchinchik <73602159+Muzhchinchik@users.noreply.github.com>
2021-09-03 12:41:11 +0300

    working tests

--- pages/landing_page.rb
+++ pages/landing_page.rb
@@ -2,5 +2,11 @@ class LandingPage < BasePage
   def sign_in_button
     browser.find_element(class: 'login')
   end
+  def contact_us_button
+    browser.find_element(id: 'contact-link')
+  end
+  def landing_page
+    browser.find_element(class: 'homefeatured')
+  end
 end
 
