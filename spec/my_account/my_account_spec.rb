747e33318ce2a52784355ace49a243a025f33165
Katerina <belenooova@gmail.com>
2021-09-03 14:16:18 +0300

    New button tests

--- spec/my_account/my_account_spec.rb
+++ spec/my_account/my_account_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 MY_ACCOUNT_PAGE = 'http://automationpractice.com/index.php?controller=my-account'
 my_account_page = MyAccountPage.new
+sign_in_page = SignInPage.new
+landing_page = LandingPage.new
+
 
 RSpec.shared_examples 'when button is clicked' do |button, link, page|
   it "verify that User can redirect to #{button} page" do
@@ -10,16 +13,14 @@ RSpec.shared_examples 'when button is clicked' do |button, link, page|
   end
 end
 
-RSpec.describe 'My account page' do
-  sign_in_page = SignInPage.new
-  landing_page = LandingPage.new
-
+RSpec.describe 'My account page - buttons' do
+  
   links_on_my_account = {
-    'Orders' : ['history', HistoryPage.new],
-    'Credit slips' : ['order-slip', CreditSlipsPage.new],
-    'Addresses' : ['addresses', MyAddressesPage.new],
-    'Information' : ['identity', YourPersonalInfoPage.new],
-    'My wishlists' : ['mywishlist', MyWishlistsPage.new]
+    'Orders' => ['history', HistoryPage.new],
+    'Credit slips' => ['order-slip', CreditSlipsPage.new],
+    'Addresses' => ['addresses', MyAddressesPage.new],
+    'Information' => ['identity', YourPersonalInfoPage.new],
+    'My wishlists' => ['mywishlist', MyWishlistsPage.new]
   }
 
   before(:all) do
@@ -36,5 +37,14 @@ RSpec.describe 'My account page' do
     it_behaves_like 'when button is clicked', key, value[0], value[1] 
   end
 
-end
+  it "verify that User can redirect to Landing page" do
+    my_account_page.home_icon.click
+    expect(landing_page.url_include?(HOME_PAGE)).to be(true)
+  end
 
+  it 'verify that User can redirect to "Sign Out" page' do
+    my_account_page.sign_out_button.click
+    expect(sign_in_page.page_title).to eq('AUTHENTICATION')
+  end
+
+end
