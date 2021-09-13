8eb0ba170450ec16936db4705bc4c104401e23e4
BrukAnn <kennyman143@gmail.com>
2021-09-14 00:38:04 +0300

    Adds new test and deleting negative tests (Volodimir's work). Code optimizing

--- spec/api/api_put_spec.rb
+++ spec/api/api_put_spec.rb
@@ -12,156 +12,59 @@ RSpec.describe 'PUT method checking to the any data in the body' do
 	
 	# Positive Functional Testing
 	
-	it 'verifies that username can be edited' do
+	it 'verifies that user can edit his own ID' do
+		body[:id] = 144
+		api_cl.update_user(body[:id], body)
+		api_cl.user_login(body[:username], body[:password])
+		response = api_cl.get_user(body[:username])
+		expect(response.status).to eq(200)
+	end
+	
+	it 'verifies that user can logged in by using new username' do
 		body[:username] = 'KingOfTheNorth'
-		response = api_cl.update_user(body[:username], body)
+		api_cl.update_user(body[:username], body)
+		api_cl.user_login(body[:username], body[:password])
+		response = api_cl.get_user(body[:username])
 		expect(response.status).to eq(200)
 	end
 	
 	it 'verifies that first name can be edited' do
 		body[:firstName] = 'Robb'
-		response = api_cl.update_user(body[:username], body)
+		api_cl.update_user(body[:firstName], body)
+		api_cl.user_login(body[:username], body[:password])
+		response = api_cl.get_user(body[:username])
 		expect(response.status).to eq(200)
 	end
 	
 	it 'verifies that last name can be edited' do
 		body[:lastName] = 'Stark'
-		response = api_cl.update_user(body[:username], body)
+		api_cl.update_user(body[:lastName], body)
+		api_cl.user_login(body[:username], body[:password])
+		response = api_cl.get_user(body[:username])
 		expect(response.status).to eq(200)
 	end
 	
 	it 'verifies that email can be edited' do
 		body['email'] = 'robb-stark@gameofthrones.com'
-		response = api_cl.update_user(body[:username], body)
+		api_cl.update_user(body[:email], body)
+		api_cl.user_login(body[:username], body[:password])
+		response = api_cl.get_user(body[:username])
 		expect(response.status).to eq(200)
 	end
 	
 	it 'verifies that password can be edited' do
-		body[:password] = 'rob-stark@gameofthrones.com'
-		response = api_cl.update_user(body[:username], body)
+		body[:password] = '123456abc'
+		api_cl.update_user(body[:password], body)
+		api_cl.user_login(body[:username], body[:password])
+		response = api_cl.get_user(body[:username])
 		expect(response.status).to eq(200)
 	end
 	
 	it 'verifies that phone number can be edited' do
 		body[:phone] = rand(100000).to_s
-		response = api_cl.update_user(body[:username], body)
+		api_cl.update_user(body[:phone], body)
+		api_cl.user_login(body[:username], body[:password])
+		response = api_cl.get_user(body[:username])
 		expect(response.status).to eq(200)
-	end
-	
-	# Negative functional testing
-	# Username
-	it 'verifies that username can not be less than 2 symbols' do
-		body[:username] = 'u'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	# First name
-	it 'verifies that first name can not be less than 2 symbols' do
-		body[:firstName] = 'O'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	it 'verifies that first name can not consist numbers' do
-		body[:firstName] = '1232151'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	it 'verifies that first name can not consist symbols' do
-		body[:firstName] = '(*#&%&'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	it 'verifies that first name can not start with low letter' do
-		body[:firstName] = 'robb'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	it 'verifies that last name can not consist mixed data' do
-		body[:firstName] = 'robbA&%34%^'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	# Last name
-	it 'verifies that last name can not be less than 2 symbols' do
-		body[:lastName] = 'R'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	it 'verifies that last name can not consist numbers' do
-		body[:lastName] = '123'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	it 'verifies that last name can not consist symbols' do
-		body[:lastName] = '((($!'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	it 'verifies that last name can not start with low letter' do
-		body[:lastName] = 'stark'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	it 'verifies that first name can not consist mixed data' do
-		body[:lastName] = 'stark134@*$AA'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	# Email
-	it 'verifies that email can not be less than 2 symbols' do
-		body[:email] = 'R'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	it 'verifies that email can not consist symbols' do
-		body[:email] = '(%#@^^'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	it 'verifies that email can not consist mixed data' do
-		body[:lastName] = 'rstark#%%@11@*$AA'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	it 'verifies that email can not be created without @post.com' do
-		body[:lastName] = 'rob-stark'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	# Password
-	
-	it 'verifies that password can not be less than 8 symbols' do
-		body[:password] = '1234'
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	it 'verifies that password can not be more than 50 symbols' do
-		body[:password] = '1' * 51
-		response = api_cl.update_user(body[:username], body)
-		!expect(response.status).to eq(200)
-	end
-	
-	
-	
-	
-	
-	
-	
-	
+	end	
 end
