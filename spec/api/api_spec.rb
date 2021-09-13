b82e35cb83fd52a61c9b07e9af2b4b8e6e7b03c8
Kytaihora Volodymyr <kytaihora.v@gmail.com>
2021-09-13 09:14:45 +0300

    add new method

--- spec/api/api_spec.rb
+++ spec/api/api_spec.rb
@@ -1,7 +1,6 @@
 require_relative '../spec_helper'
 require_relative 'api_client.rb'
 
-
 RSpec.describe 'Create -> Login -> Get' do
 
     app_cl = ApiClient.new
@@ -29,6 +28,16 @@ RSpec.describe 'Create -> Login -> Get' do
         it 'verifies user can get info' do
             response = app_cl.get_user(body[:username])
             expect(response.status).to eq(200)
+        end
+    end
+
+ ###################################################################
+
+    context 'verify updating user info with invalid data' do
+        it 'verifies user can update invalid ID' do
+            new = {:id => -3}
+            response = app_cl.update_user(body[:username], body.merge!(new){|key, b, n| b = n})
+            expect(response.status).to eq(400)
         end
     end
 end
