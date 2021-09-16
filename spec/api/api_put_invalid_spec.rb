b4f5371f17899f2abd3a012a353a422e32f2af35
Kytaihora Volodymyr <kytaihora.v@gmail.com>
2021-09-16 22:48:05 +0300

    api_put_v2

--- spec/api/api_put_invalid_spec.rb
+++ spec/api/api_put_invalid_spec.rb
@@ -26,47 +26,31 @@ RSpec.describe 'Create -> Login -> Get' do
         end
     end
 
-
+    dataID = {
+        "negative values": -5,
+        "empty field": " ",
+        "specials symbols": "@/#$)",
+        "length 50 symbols": "k" * 50,
+        "mixed values": "@#-5/Dark"
+    }
+    
     context 'verify user can update ID' do
-        it 'verifies user can update with negative values' do
-            #new = {id: -3}
-            body[:id] = -5
-            #response = app_cl.update_user(body[:username], app_cl.adjust_body(new))
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(400)
-        end
 
-        it 'verifies user can update with empty field' do
-            body[:id] = ""
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(400)
-        end
-
-        it 'verifies user can update with specials symbols' do
-            body[:id] = '@/#$)'
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(400)
-        end
-
-        it 'verifies user can update with length 50 symbols' do
-            body[:id] = '3' * 50
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(400)
+        dataID.each_pair do |key, value| 
+            it "verifies user can update with #{key}" do
+                body[:id] = value
+                response = app_cl.update_user(body[:username], body)
+                expect(response.status).to eq(500)
+            end            
         end
     end
-
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  
     context 'verify user can update USER NAME' do
+
         it 'verifies user can not update username with empty field' do
             invalid_username = ''
-            #body = app_cl.generate_random_body
-            #app_cl.create_user(body)
-
-            body = app_cl.generate_random_body
             response = app_cl.update_user(body[:username], app_cl.adjust_body(username: invalid_username))
             expect(response.status).to eq(400)
-            user = app_cl.get_user(body[:username])
-            expect(user[:username]).to eq(body[:username])
         end
 
         it 'verifies user can not update username with negative values' do
@@ -82,7 +66,8 @@ RSpec.describe 'Create -> Login -> Get' do
         end
 
         it 'verifies user can not update username with mixed values' do
-            invalid_username = '@#-/Dark'
+            invalid_username = '@#-/1Dark'
+            #invalid_username = SecureRandom.base64(10)
             response = app_cl.update_user(body[:username], app_cl.adjust_body(username: invalid_username))
             expect(response.status).to eq(400)
         end
@@ -94,206 +79,100 @@ RSpec.describe 'Create -> Login -> Get' do
         end
     end
 
+    data = {
+        "1 symbol": 'F',
+        "empty field": " ",
+        "specials symbols": "@/#$)",
+        "50 letters": 'k' * 50,
+        "numerical values": 123456,
+        "mixed values": '@#-/1Dark'
+    }
 
     context 'verify user can update FIRST NAME' do
-        it 'verifies user can update with 1 symbol' do
-            body[:firstName] = 'F'
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(400)
-        end
-
-        it 'verifies user can update with empty field' do
-            body[:firstName] = ""
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(400)
-        end
-
-        it 'verifies user can update with specials symbols' do
-            body[:firstName] = '@/#$)'
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(400)
-        end
-
-        it 'verifies user can update with 50 letters' do
-            body[:firstName] = 'X' * 50
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(400)
-        end
-
-        it 'verifies user can update with numerical values' do
-            body[:firstName] = 123456
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(400)
+        
+        data.each_pair do |key, value|
+            it "verifies user can update with #{key}" do
+                body[:firstName] = value
+                response = app_cl.update_user(body[:username], body)
+                expect(response.status).to eq(500)
+            end
         end
     end
 
 
+
     context 'verify user can update LAST NAME' do
-        it 'verifies user can update with 1 symbol' do
-            body[:lastName] = 'F'
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
 
-        it 'verifies user can update with empty field' do
-            body[:lastName] = ""
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
-
-        it 'verifies user can update with specials symbols' do
-            body[:lastName] = '@/#$)'
-            response = app_cl.update_user(body[:lusername], body)
-            expect(response.status).to eq(500)
-        end
-
-        it 'verifies user can update with 50 letters' do
-            body[:lastName] = 'X' * 50
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
-
-        it 'verifies user can update with numerical values' do
-            body[:lastName] = 123456
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
+        data.each_pair do |key, value|
+            it "verifies user can update with #{key}" do
+                body[:lastName] = value
+                response = app_cl.update_user(body[:username], body)
+                expect(response.status).to eq(500)
+            end
         end
     end
 
 
     context 'verify user can update EMAIL' do
-        it 'verifies user can update with 1 symbol' do
-            body[:email] = 'F'
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
 
-        it 'verifies user can update with empty field' do
-            body[:email] = ""
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
-
-        it 'verifies user can update with specials symbols' do
-            body[:email] = '@/#$)'
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
-
-        it 'verifies user can update with 50 letters' do
-            body[:email] = 'X' * 50
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
-
-        it 'verifies user can update with numerical values' do
-            body[:email] = 123456
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
+        data.each_pair do |key, value|
+            it "verifies user can update with #{key}" do
+                body[:email] = value
+                response = app_cl.update_user(body[:username], body)
+                expect(response.status).to eq(500)
+            end
         end
     end
 
-
+    dataPassword = {
+        "1 symbol": 'F',
+        "empty field": " ",
+        "specials symbols": "@/#$)",
+        "50 letters": 'k' * 50,
+        "mixed values": '@#-/1Dark'
+    }
 
     context 'verify user can update PASSWORD' do
-        it 'verifies user can update with 1 symbol' do
-            body[:password] = 'F'
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
 
-        it 'verifies user can update with empty field' do
-            body[:password] = ""
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
-
-        it 'verifies user can update with specials symbols' do
-            body[:password] = '@/#$)'
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500) 
-        end
-
-        it 'verifies user can update with 50 letters' do
-            body[:password] = 'X' * 50
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
-
-        it 'verifies user can update with numerical values' do
-            body[:password] = 1
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
+        dataPassword.each_pair do |key, value| 
+            it "verifies user can update with #{key}" do
+                body[:password] = value
+                response = app_cl.update_user(body[:username], body)
+                expect(response.status).to eq(500)
+            end
         end
     end
+
 
     context 'verify user can update PHONE' do
-        it 'verifies user can update with 1 symbol' do
-            body[:password] = 'Fly'
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
 
-        it 'verifies user can update with empty field' do
-            body[:password] = ""
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
+        dataID.each_pair do |key, value|
+            it "verifies user can update with #{key}" do
+                body[:phone] = value
+                response = app_cl.update_user(body[:username], body)
+                expect(response.status).to eq(500)
+            end
         end
-
-        it 'verifies user can update with specials symbols' do
-            body[:password] = '@/#$)'
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500) 
-        end
-
-        it 'verifies user can update with 50 letters' do
-            body[:password] = 'X' * 50
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
-
-        it 'verifies user can update with numerical values' do
-            body[:password] = 1
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
-
-        it 'verifies user can update with mixed values' do
-            body[:password] = 'cre@2k'
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
+        
     end
 
+    dataStatus = {
+        "negative values": -5,
+        "empty field": " ",
+        "specials symbols": "@/#$)",
+        "length 51 symbols": "k" * 51,
+        "text": "Fly",
+        "mixed values": "@#-5/Dark"
+    }
+
     context 'verify user can update USER STATUS' do
-        it 'verifies user can update with negative values' do
-            body[:userStatus] = -5
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
 
-        it 'verifies user can update with alphabetical value' do
-            body[:userStatus] = "Netw"
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
-
-        it 'verifies user can update with empty field' do
-            body[:userStatus] = ""
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
-
-        it 'verifies user can update with specials symbols' do
-            body[:userStatus] = '@/#$)'
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
-        end
-
-        it 'verifies user can update with length 50 symbols' do
-            body[:userStatus] = '3' * 50
-            response = app_cl.update_user(body[:username], body)
-            expect(response.status).to eq(500)
+        dataStatus.each_pair do |key, value| 
+            it "verifies user can update with #{key}" do
+                body[:userStatus] = value
+                response = app_cl.update_user(body[:username], body)
+                expect(response.status).to eq(500)
+            end
         end
     end
 end
